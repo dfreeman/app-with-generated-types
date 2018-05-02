411296bc63701105f4b68bfc7af9747f390c462c
Dan Freeman <dfreeman@salsify.com>
2018-05-01 22:07:20 -0400

    Configure tsconfig and run the build to generate types

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import relativeAppTemplate from './templates/application';
+import absoluteAppTemplate from 'app-with-generated-types/templates/application';
+
+console.log('Look, I imported the template just because I could', relativeAppTemplate, absoluteAppTemplate);
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
